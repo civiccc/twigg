f0bb7d1886a8589fcb8410f96a13deb03f019854
Greg Hurrell <greg@causes.com>
2013-07-15 09:04:45 -0700

    Add block support to settings DSL
    This allows us to remove the `#repositories_directory` method, and
    replace it with a call to the DSL. This is arguably more readable,
    because `name` is somewhat more pleasant on the eye than `__method__`.
    
    Change-Id: I5499c5a733439cc3b22bdabbf35a6f71a5bd591a
    Reviewed-on: https://gerrit.causes.com/25236
    Reviewed-by: Greg Hurrell <greg@causes.com>
    Tested-by: Greg Hurrell <greg@causes.com>

--- lib/twigg/settings.rb
+++ lib/twigg/settings.rb
@@ -18,22 +18,27 @@ module Twigg
       # DSL method which creates a reader for the setting `name`. If the
       # configuration file does not contain a value for the setting, return
       # `options[:default]` from the `options` hash.
-      def setting(name, options = {})
+      #
+      # A block maybe provided to do checking of the supplied value prior to
+      # returning; for an example, see the `repositories_directory` setting in
+      # this file.
+      def setting(name, options = {}, &block)
         define_method name do
           value = instance_variable_get("@#{name}")
           return value if value
-          instance_variable_set("@#{name}", self.[](name) || options[:default])
+
+          value = self.[](name) || options[:default]
+          yield name, value if block_given?
+          instance_variable_set("@#{name}", value)
         end
       end
     end
 
     setting :bind, default: '0.0.0.0'
 
-    def repositories_directory
-      @repositories_directory ||= self.[](__method__).tap do |dir|
-        raise ArgumentError, "#{__method__} not set" unless dir
-        raise ArgumentError, "#{__method__} not a directory" unless File.directory?(dir)
-      end
+    setting :repositories_directory do |name, value|
+      raise ArgumentError, "#{name} not set" unless value
+      raise ArgumentError, "#{name} not a directory" unless File.directory?(value)
     end
   end
 end
