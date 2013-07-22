b45c916e4e029ccdcb63986702e1bdc4d212d18e
Greg Hurrell <greg@causes.com>
2013-07-21 21:51:24 -0700

    Teach web app to link back to GitHub for individual commits
    Whenever `github.organization` is set, construct links to GitHub commits
    for each commit shown in the web UI.
    
    I added a `commit` accessor to the `Twigg::Commit` class rather than
    accessing the instance variable directly for parity with the other
    accessors in that class, and also for the nice touch of showing the full
    commit SHA-1 in the web UI on mouseover (as a `title` attribute).
    
    Change-Id: I6accede3deb0a7afbd45430b53a48b71b8cd7468
    Reviewed-on: https://gerrit.causes.com/25300
    Reviewed-by: Greg Hurrell <greg@causes.com>
    Tested-by: Greg Hurrell <greg@causes.com>

--- lib/twigg/commit.rb
+++ lib/twigg/commit.rb
@@ -1,6 +1,6 @@
 module Twigg
   class Commit
-    attr_reader :repo, :subject, :author, :date, :stat
+    attr_reader :repo, :commit, :subject, :author, :date, :stat
 
     def initialize(options)
       raise ArgumentError unless @repo    = options[:repo]
@@ -9,6 +9,12 @@ module Twigg
       raise ArgumentError unless @author  = options[:author]
       raise ArgumentError unless @date    = options[:date]
       raise ArgumentError unless @stat    = options[:stat]
+    end
+
+    def link
+      if Config.github.organization
+        "https://github.com/#{Config.github.organization}/#{repo.name}/commit/#{commit}"
+      end
     end
 
     def author_names
