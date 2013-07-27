2cda628e541d350d4aae22df6b98601ccb1ebeb9
Greg Hurrell <greg@causes.com>
2013-07-27 08:48:55 -0700

    Use `#tap` to avoid managing a temporary local variable
    Let the block syntax handle that for us.
    
    Change-Id: I8dc02aba8fc434cd008e83fb657ea78d31ecbdc2
    Reviewed-on: https://gerrit.causes.com/25837
    Reviewed-by: Greg Hurrell <greg@causes.com>
    Tested-by: Greg Hurrell <greg@causes.com>

--- twigg/lib/twigg/gatherer.rb
+++ twigg/lib/twigg/gatherer.rb
@@ -3,17 +3,17 @@ module Twigg
     def self.gather(repositories_directory, days)
       since = Time.now - days * 24 * 60 * 60
 
-      commit_set = CommitSet.new
-      Dir[File.join(repositories_directory, '*')].each do |repo_path|
-        begin
-          Repo.new(repo_path).commits(since: since).each do |commit|
-            commit_set << commit
+      CommitSet.new.tap do |commit_set|
+        Dir[File.join(repositories_directory, '*')].each do |repo_path|
+          begin
+            Repo.new(repo_path).commits(since: since).each do |commit|
+              commit_set << commit
+            end
+          rescue Repo::InvalidRepoError
+            # most likely an empty or non-Git directory
           end
-        rescue Repo::InvalidRepoError
-          # most likely an empty or non-Git directory
         end
       end
-      commit_set
     end
   end
 end
