354a5048e0458d3d4c1e639b38fc1629ce10ad61
Greg Hurrell <greg@causes.com>
2013-08-04 22:45:42 -0700

    Simplify `PairMatrix#pairs` by extracting a `#sparse_matrix` method
    Get rid of some of the clutter form the `PairMatrix#pairs` method by
    extracting a `#sparse_matrix` method. This makes the `#pairs` method
    more readable, and, as a bonus, provides us with a space to document
    what the sparse-matrix logic is doing.
    
    Change-Id: Icfc72ee9e672496cc04031cf3683a3a68dd4c104
    Reviewed-on: https://gerrit.causes.com/26182
    Reviewed-by: Greg Hurrell <greg@causes.com>
    Tested-by: Greg Hurrell <greg@causes.com>

--- twigg/lib/twigg/pair_matrix.rb
+++ twigg/lib/twigg/pair_matrix.rb
@@ -10,9 +10,7 @@ module Twigg
     end
 
     def pairs
-      @pairs ||= Hash.new do |hash, pairer|
-        hash[pairer] = Hash.new { |hash, pairee| hash[pairee] = 0 }
-      end.tap do |matrix|
+      @pairs ||= sparse_matrix.tap do |matrix|
         @commit_set.each do |commit|
           authors = commit.author_names
 
@@ -43,6 +41,21 @@ module Twigg
         [pairs.inject(0) do |max, (pairer, count)|
           [pairee == pairer ? 0 : count, max].max
         end, max].max
+      end
+    end
+
+  private
+
+    # Returns a Hash instance that models a sparse matrix.
+    #
+    # Looking up a pairee/pairer pair in the matrix returns 0 if the matrix does
+    # not have a value for that entry; for example:
+    #
+    #     sparse_matrix['Jimmy Kittiyachavalit']['Chris Chan'] #=> 0
+    #
+    def sparse_matrix
+      Hash.new do |hash, pairer|
+        hash[pairer] = Hash.new { |hash, pairee| hash[pairee] = 0 }
       end
     end
   end
