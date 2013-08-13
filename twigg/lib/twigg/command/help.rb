bf3caa1f43e4bfa5b1138afb82b67cc1f79a3b3c
Greg Hurrell <greg@causes.com>
2013-08-12 22:58:24 -0700

    Further DRY up help subcommand emission
    This makes it even easier to add help for new subcommands.
    
    Along the way I made the output format more compact (no blank lines
    under topic headers, and no indenting); this was initially just going to
    be an intermediate state on the way to maintaining parity with what the
    output looked like before, but on inspecting it while testing, I found
    it to look better so am going to leave it.
    
    Change-Id: I844fb3f6ccc0157e509279502017dfaead8b95cb
    Reviewed-on: https://gerrit.causes.com/26638
    Reviewed-by: Greg Hurrell <greg@causes.com>
    Tested-by: Greg Hurrell <greg@causes.com>

--- twigg/lib/twigg/command/help.rb
+++ twigg/lib/twigg/command/help.rb
@@ -25,99 +25,86 @@ module Twigg
         Shellwords.escape($0)
       end
 
+      TOPIC_HEADERS = Hash.new { |h, k| h[k] = k.capitalize }.merge(
+        # header = subcommand with first letter capitalized; exceptions:
+        'app'    => 'Web application',
+        'github' => 'GitHub',
+      )
+
       def show_help(topic)
+        puts TOPIC_HEADERS[topic] + ':'
         stderr strip_heredoc(send(topic)) + "\n"
       end
 
       def app
         <<-DOC
-          Web application:
-
-            #{executable} app
+          #{executable} app
         DOC
       end
 
       def commands
         <<-DOC
-          Commands:
-
-            #{executable} app    # run the Twigg web app
-            #{executable} gerrit # clone/update/report from Gerrit
-            #{executable} git    # perform operations on Git repos
-            #{executable} github # clone/update from GitHub
-            #{executable} init   # generate a .twiggrc file
-            #{executable} help   # this help information
-            #{executable} stats  # show statistics about repos
+          #{executable} app    # run the Twigg web app
+          #{executable} gerrit # clone/update/report from Gerrit
+          #{executable} git    # perform operations on Git repos
+          #{executable} github # clone/update from GitHub
+          #{executable} init   # generate a .twiggrc file
+          #{executable} help   # this help information
+          #{executable} stats  # show statistics about repos
         DOC
       end
 
       def gerrit
         <<-DOC
-          Gerrit:
-
-            #{executable} gerrit clone [repos dir]  # clone repos into repos dir
-            #{executable} gerrit update [repos dir] # update repos in repos dir
-            #{executable} gerrit stats [repos dir]  # show stats for repos in dir
+          #{executable} gerrit clone [repos dir]  # clone repos into repos dir
+          #{executable} gerrit update [repos dir] # update repos in repos dir
+          #{executable} gerrit stats [repos dir]  # show stats for repos in dir
         DOC
       end
 
       def git
         <<-DOC
-          Git:
-
-            #{executable} git gc [repos dir] # garbage collect repos in repos dir
+          #{executable} git gc [repos dir] # garbage collect repos in repos dir
         DOC
       end
 
       def github
         <<-DOC
-          GitHub:
-
-            #{executable} github clone [repos dir]  # clone repos into repos dir
-            #{executable} github update [repos dir] # update repos in repos dir
+          #{executable} github clone [repos dir]  # clone repos into repos dir
+          #{executable} github update [repos dir] # update repos in repos dir
         DOC
       end
 
       def help
         <<-DOC
-          Help:
-
-            #{executable} help              # this help information
-            #{executable} help <subcommand> # help for a specific subcommand
-            #{executable} help commands     # list all subcommands
+          #{executable} help              # this help information
+          #{executable} help <subcommand> # help for a specific subcommand
+          #{executable} help commands     # list all subcommands
         DOC
       end
 
       def init
         <<-DOC
-          Init:
-
-            #{executable} init # emit a sample .twiggrc file to standard out
+          #{executable} init # emit a sample .twiggrc file to standard out
         DOC
       end
 
       def russian
         <<-DOC
-          Russian:
-
-            #{executable} russian <repos dir> <number of days> # easter egg
+          #{executable} russian <repos dir> <number of days> # easter egg
         DOC
       end
 
       def stats
         <<-DOC
-          Stats:
-
-            #{executable} stats [--verbose|-v] <repos dir> <number of days>
+          #{executable} stats [--verbose|-v] <repos dir> <number of days>
         DOC
       end
 
       def usage
         <<-DOC
-          Usage:
-
-            #{executable} <subcommand> [options] <arguments...>
-            #{executable} help
+          #{executable} <subcommand> [options] <arguments...>
+          #{executable} help
         DOC
       end
     end
