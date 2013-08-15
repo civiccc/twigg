d20539ae92deba028ddef171a869e644a8669b13
Greg Hurrell <greg@causes.com>
2013-08-15 01:22:38 -0700

    Apply minor tweaks to improve appearance of commit bar chart
    This makes things line up a little nicely and fixes some clipping
    issues. It's still not perfect, but it is relatively presentable at this
    point.
    
    Change-Id: Iae39e3be0a8a3cb9632d1b2d4d4fcfd695bf0f3a
    Reviewed-on: https://gerrit.causes.com/26652
    Reviewed-by: Greg Hurrell <greg@causes.com>
    Tested-by: Greg Hurrell <greg@causes.com>

--- twigg-app/public/application.js
+++ twigg-app/public/application.js
@@ -5,9 +5,9 @@ Twigg.Profile = (function() {
         width = $('#bar-chart').width(),
         padding = 45,
         barSpacing = 1,
-        barWidth = width / data.length - barSpacing,
+        barWidth = (width - padding) / data.length - barSpacing,
         x = d3.scale.linear()
-          .domain([0,data.length])
+          .domain([0, data.length])
           .range([0, width]),
         y = d3.scale.linear()
           .domain([0, d3.max(data, function(d) { return d.count; })])
@@ -37,7 +37,7 @@ Twigg.Profile = (function() {
         .append('rect')
         .attr({
           'class': 'bar',
-          'x': function(d, i) { return x(i) },
+          'x': function(d, i) { return x(i) + padding },
           'y': function(d) { return y(d.count) },
           'width': barWidth,
           'height': function(d) { return height - y(d.count); },
@@ -52,7 +52,7 @@ Twigg.Profile = (function() {
       svg.selectAll('text').data(data)
         .enter()
           .append('text')
-          .attr('x', function(d, i) { return x(i) + barWidth; })
+          .attr('x', function(d, i) { return x(i) + barWidth + padding; })
           .attr('y', function(d, i) { return y(d.count); })
           .attr('dx', -barWidth / 2)
           .attr('dy', '1.2em')
@@ -64,13 +64,13 @@ Twigg.Profile = (function() {
     // x-axis
     svg.append('g')
       .attr('class', 'xaxis axis')
-      .attr('transform', 'translate(0, ' + height + ')')
+      .attr('transform', 'translate(' + padding + ', ' + height + ')')
       .call(xAxis);
 
     // y-axis
     svg.append('g')
       .attr('class', 'yaxis axis')
-      .attr('transform', 'translate(30,0)')
+      .attr('transform', 'translate(' + padding + ', 0)')
       .call(yAxis)
       .append('text')
         .attr('transform', 'rotate(-90)')
@@ -79,10 +79,11 @@ Twigg.Profile = (function() {
         .style('text-anchor', 'end')
         .text('Commits');
 
+    // date labels
     svg.selectAll('.xaxis text')
       .attr('transform', function(d) {
         var h = this.getBBox().height;
-        return 'translate(' + h * -1 + ', ' + h + ') rotate(-45)';
+        return 'translate(' + h * -1 + ', ' + h + ') rotate(-30)';
       });
   };
 
