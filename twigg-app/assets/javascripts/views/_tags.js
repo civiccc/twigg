027fe25bc20c9327664c72bf09cb0bbf00a3a1d2
Greg Hurrell <greg@causes.com>
2013-08-20 23:37:57 -0700

    Show a progress bar while loading tag stats
    This progress bar is shown during loading, removed on success, or
    replaced with an error "flash" on failure.
    
    Change-Id: I24ecd7ef9dcb4c8f9b79ad584b0503e816b894c1
    Reviewed-on: https://gerrit.causes.com/27203
    Reviewed-by: Greg Hurrell <greg@causes.com>
    Tested-by: Greg Hurrell <greg@causes.com>

--- twigg-app/assets/javascripts/views/_tags.js
+++ twigg-app/assets/javascripts/views/_tags.js
@@ -3,8 +3,15 @@
 // Manages a set of TagsWordCloud subviews, one per word cloud.
 Twigg.Views.Tags = Backbone.View.extend({
   initialize: function() {
+    this.$globalUsed = $('#global-used');
+    this.fetch();
+  },
+
+  fetch: function() {
     $.getJSON(window.location.href)
+      .fail(function() { this.$globalUsed.displayAlert(); }.bind(this))
       .done(function(data) {
+        this.$globalUsed.empty();
         this.data = data;
         this.render();
       }.bind(this));
@@ -12,7 +19,7 @@ Twigg.Views.Tags = Backbone.View.extend({
 
   render: function() {
     var subview = new Twigg.Views.TagsWordCloud({
-      el:   $('#global-used')[0],
+      el:   this.$globalUsed[0],
       data: this.data
     });
 
